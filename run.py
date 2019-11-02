be13211f6c6b9c56f11a6781391107e24a62718b
Peter Ryder <peter.ryder2@gmail.com>
2019-11-02 00:54:22 +0000

    first commit

---
+++ run.py
@@ -0,0 +1,7 @@
+import numpy as numpy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource,Api
+app = Flask(__name__)
+import sys
+print(sys.executable)
